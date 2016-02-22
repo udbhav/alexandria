db043530ef87f0ef21f6648cf4a8ac5db0529f58
Udbhav Gupta <dev@outdoorvoices.com>
2016-02-22 16:35:56 -0500

    use mizra

--- src/alexandria/settings.py
+++ src/alexandria/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'alexandria.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mizra',
+        'USER': 'vagrant',
+        'PORT': '5432',
     }
 }
 
@@ -108,3 +109,8 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_URL = "/admin/login/"
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
